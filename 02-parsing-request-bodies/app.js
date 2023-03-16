8e57b4fc1c5a4a2719667ddd6651be04a9f5cb88
Ha Anna <its.haanna@gmail.com>
2023-03-16 21:55:21 +0900

    Fix: typo

--- 02-parsing-request-bodies/app.js
+++ 02-parsing-request-bodies/app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   const method = req.method;
   if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
+    res.write('<head><title>Enter Message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end();
