bd68208b1df291258ff248114101a8fe59d6a4c9
Ha Anna <its.haanna@gmail.com>
2023-03-16 23:17:21 +0900

    Upload: improved version

--- M1: Basics/05-assignment/routes.js
+++ M1: Basics/05-assignment/routes.js
@@ -2,40 +2,43 @@ const fs = require('fs')
 
 const requestHandler = (req, res) => {
   const url = req.url
-  const method = req.method
 
   if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html')
     res.write("<!DOCTYPE html> <html lang='en'>")
     res.write("<head><meta charset='UTF-8'><title>Hello!</title></head>")
     res.write("<body>")
     res.write("<h1>Hello there!</h1>")
-    res.write("<form action='/users' method='POST'><input type='text' name='user'/><button type='submit'>Send</button></form>")
+    res.write("<form action='/create-user' method='POST'><input type='text' name='username' placeholder='Username...'/><button type='submit'>Send</button></form>")
     res.write("</body>")
     res.write("</html>")
-    return res.end();
+    return res.end()
   }
-  if (url === '/users' && method === 'POST') {
+  if (url === '/users') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write("<!DOCTYPE html> <html lang='en'>")
+    res.write("<head><meta charset='UTF-8'><title>Users</title></head>")
+    res.write("<body>")
+    res.write("<h1>Users:</h1>")
+    res.write("<ul><li>User 1</li><li>User 2</li><li>User 3</li></ul>")
+    res.write("</body>")
+    res.write("</html>")
+    return res.end()
+  }
+  if (url === '/create-user') {
     const body = []
     req.on('data', chunk => {
       body.push(chunk)
     })
-    return req.on('end', () => {
+    req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString()
       const user = parsedBody.split('=')[1]
       console.log(user)
-      res.write("<!DOCTYPE html> <html lang='en'>")
-      res.write("<head><meta charset='UTF-8'><title>Users</title></head>")
-      res.write("<body>")
-      res.write("<h1>Users:</h1>")
-      res.write("<ul><li>User 1</li><li>User 2</li><li>User 3</li></ul>")
-      res.write("</body>")
-      res.write("</html>")
-      return res.end();
     })
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    res.end()
   }
 }
-
-
 
 module.exports = requestHandler
