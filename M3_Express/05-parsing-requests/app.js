d16c7bcc91ecd2250e55297a80445de0d32a0f11
Ha Anna <its.haanna@gmail.com>
2023-03-19 14:01:55 +0900

    Upload: lesson 5, lesson 6

--- M3_Express/05-parsing-requests/app.js
+++ M3_Express/05-parsing-requests/app.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
+    console.log('This always runs!')
+    next()
 });
 
 app.use('/add-product', (req, res, next) => {
   console.log('In another middleware!');
-  res.send('<form action="product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>');
+  res.send('<form action="product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>')
 });
 
 app.post('/product', (req, res, next) => {
@@ -21,8 +21,8 @@ app.post('/product', (req, res, next) => {
 })
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+  console.log('In another middleware!')
+  res.send('<h1>Hello from Express!</h1>')
 });
 
 app.listen(3000);
