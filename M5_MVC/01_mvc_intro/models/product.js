5f4c5d76c55e214fd5d1c78ce5a857cfaffd2365
Ha Anna <its.haanna@gmail.com>
2023-03-24 15:43:29 +0900

    Fix: refactoring the code

--- M5_MVC/01_mvc_intro/models/product.js
+++ M5_MVC/01_mvc_intro/models/product.js
@@ -2,17 +2,23 @@ const fs = require('fs')
 const path = require('path')
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
 
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([])
+      } else {
+        cb(JSON.parse(fileContent))
+      }
+    })
+}
+
 module.exports = class Product {
   constructor(t) {
     this.title = t
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = []
-      if (!err) {
-        products = JSON.parse(fileContent)
-      }
+    getProductsFromFile(products => {
       products.push(this)
       fs.writeFile(p, JSON.stringify(products), err => {
         if (err) {
@@ -23,12 +29,7 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([])
-      }
-      cb(JSON.parse(fileContent))
-    })
+    getProductsFromFile(cb)
   }
 
 }
